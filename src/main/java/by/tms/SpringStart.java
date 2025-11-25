435e3996fb4e5889f64c0491ee54e1672a7690ac
awwyxlove <zhanminskzhan@gmail.com>
2025-11-25 12:35:46 +0300

    Swagger

--- src/main/java/by/tms/SpringStart.java
+++ src/main/java/by/tms/SpringStart.java
@@ -1,8 +1,24 @@
 package by.tms;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "App for learn Spring",
+                description = "We use this app to learn Java:)",
+                version = "1.0.0",
+                contact = @Contact(
+                        name = "Zhan",
+                        email = "zhanminskzhan@gmail.com",
+                        url = "https://github.com/soulofz"
+                )
+        )
+)
 @SpringBootApplication
 public class SpringStart {
     public static void main(String[] args) {
--- src/main/java/by/tms/model/UserCreateDto.java
+++ src/main/java/by/tms/model/UserCreateDto.java
@@ -1,7 +1,8 @@
 package by.tms.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
-
+@Schema(description = "Это пользователь для создания в системе")
 @Data
 public class UserCreateDto {
     private String firstName;
